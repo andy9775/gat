d903c73375ca20fa922fb4464fde0e1c05d494a6
Andy Chrzaszcz <andy9775@gmail.com>
2018-12-25 19:09:01 -0500

    configure npm token for publish step

--- actions/npm-publish/entrypoint.sh
+++ actions/npm-publish/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# source: https://github.com/actions/npm
+NPM_CONFIG_USERCONFIG="$HOME/.npmrc"
+NPM_REGISTRY_URL="registry.npmjs.org"
+
+printf "//$NPM_REGISTRY_URL/:_authToken=$NPM_TOKEN\nregistry=$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+chmod 0600 "$NPM_CONFIG_USERCONFIG"
+
 cd $GITHUB_WORKSPACE
 node /index.js
-yarn publish --non-interactive --access public
+npm publish --access public
