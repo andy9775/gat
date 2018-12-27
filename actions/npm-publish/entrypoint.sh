56849337cd3be792638384e2bf88fcd5251e121c
Andy Chrzaszcz <andy9775@gmail.com>
2018-12-27 15:11:38 -0500

    Update npm publish step
    Ensure npm registry is configured correctly including auth token
    Don't yarn build as it's setup as a pre-publish step

--- actions/npm-publish/entrypoint.sh
+++ actions/npm-publish/entrypoint.sh
@@ -5,9 +5,11 @@ if [[ $GITHUB_EVENT_NAME == "release" ]]; then
   # ====================================== config ======================================
   # source: https://github.com/actions/npm
   NPM_CONFIG_USERCONFIG="$HOME/.npmrc"
-  NPM_REGISTRY_URL="http://registry.npmjs.org"
+  NPM_REGISTRY_URL="registry.npmjs.org"
 
-  printf "//$NPM_REGISTRY_URL/:_authToken=$NPM_TOKEN\nregistry=$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+  printf "registry=https://$NPM_REGISTRY_URL" >> "$NPM_CONFIG_USERCONFIG"
+  printf "\n" >> "$NPM_CONFIG_USERCONFIG"
+  printf "//$NPM_REGISTRY_URL/:_authToken=$NPM_TOKEN" >> "$NPM_CONFIG_USERCONFIG"
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 
   # ====================================== publish ======================================
@@ -17,9 +19,8 @@ if [[ $GITHUB_EVENT_NAME == "release" ]]; then
   echo "publishing version $VERSION"
 
   yarn install
-  yarn build
 
-  # yarn has bugs publishing without interactions
+  # yarn has bugs publishing without interactions - use npm
   # don't create a git tag
   npm config set git-tag-version false
   npm version $VERSION
