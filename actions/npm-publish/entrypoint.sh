3f616bae44df432dafa1636d38ebd1a1fe7c1e36
Andy Chrzaszcz <andy9775@gmail.com>
2018-12-27 00:45:41 -0500

    update publish action and prevent a git tag from being created

--- actions/npm-publish/entrypoint.sh
+++ actions/npm-publish/entrypoint.sh
@@ -5,14 +5,16 @@ if [[ $GITHUB_EVENT_NAME == "release" ]]; then
   # ====================================== config ======================================
   # source: https://github.com/actions/npm
   NPM_CONFIG_USERCONFIG="$HOME/.npmrc"
-  NPM_REGISTRY_URL="registry.npmjs.org"
+  NPM_REGISTRY_URL="http://registry.npmjs.org"
 
   printf "//$NPM_REGISTRY_URL/:_authToken=$NPM_TOKEN\nregistry=$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 
   # ====================================== publish ======================================
   VERSION=${GITHUB_REF##*/v}
-  
+
+  npm config set git-tag-version false
+
   cd $GITHUB_WORKSPACE
   echo "publishing version $VERSION"
   npm version $VERSION
